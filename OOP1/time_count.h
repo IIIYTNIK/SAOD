c8045f3737406b01faf9105bcfa1c96d0b70134d
IIIYTNIK <denborodienko@yandex.ru>
2025-04-02 14:42:37 +0900

    #Русские комментарии

--- OOP1/time_count.h
+++ OOP1/time_count.h
@@ -11,7 +11,7 @@ namespace time_count {
     int measure_time(Func func) {
         auto t1 = std::chrono::steady_clock::now(); //текущее время до выполнения кода
         func(); //выполнения некоторого кода
-        auto t2 = std::chrono::steady_clock::now(); //текущее время после выполнения кода
+        auto t2 = std::chrono::steady_clock::now(); //текущее время после выполнения кода.
 
         auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1); // разница времени 
         cout << "Time milliseconds: " << delta.count() << endl;
